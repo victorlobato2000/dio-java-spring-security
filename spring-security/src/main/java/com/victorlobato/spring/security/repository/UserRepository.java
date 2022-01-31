6ead9b4d46cf0f84c83a7b6ad11d752a8d3ff302
Victor Lobato <victorsilva123190@gmail.com>
2022-01-31 19:03:47 -0300

    Adicionando UserRepository

---
+++ spring-security/src/main/java/com/victorlobato/spring/security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.victorlobato.spring.security.repository;
+
+import com.victorlobato.spring.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+}
