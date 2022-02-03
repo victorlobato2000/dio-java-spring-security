37394803cbd1eaacccb5c19459e859508f0ce0ac
Victor Lobato <victorsilva123190@gmail.com>
2022-02-03 13:08:44 -0300

    Criando autenticação com banco de dados

--- spring-security/src/main/java/com/victorlobato/spring/security/WebSecurityConfig.java
+++ spring-security/src/main/java/com/victorlobato/spring/security/WebSecurityConfig.java
@@ -1,16 +1,26 @@
 package com.victorlobato.spring.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SecurityDatabaseService securityService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder((NoOpPasswordEncoder.getInstance()));
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,9 +29,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                  .antMatchers("/login").permitAll()
                  .antMatchers("/managers").hasAnyRole("MANAGERS")
                  .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
-                 .anyRequest().authenticated().and().formLogin();
+                 .anyRequest().authenticated().and().httpBasic();
     }
 
+    /*
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -33,4 +44,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{noop}master123")
                 .roles("MANAGERS");
     }
+     */
 }
--- spring-security/src/main/java/com/victorlobato/spring/security/WelcomeController.java
+++ spring-security/src/main/java/com/victorlobato/spring/security/WelcomeController.java
@@ -1,6 +1,5 @@
 package com.victorlobato.spring.security;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
